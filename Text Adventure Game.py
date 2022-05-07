0d38aa93845cc3ff4b24519c4a719e2c03e4a8a4
e. alvarez <mail@alvar3z.com>
2022-05-07 19:55:45 -0400

    made some changes. read the PR request

--- Text Adventure Game.py
+++ Text Adventure Game.py
@@ -1,56 +1,59 @@
+#!/usr/bin/env python3
+
 import os
 import time
 import sys
 import random
 
+# ---------------- GLOBAL VARS -------------------
 currentEnemy = 'Apostle'
-#a global variable to determine how many attempts the user has to pick the correct option
-attemptCounter = -1 
-#To change the properties of different pieces of text 
-#for example print(color.BOLD + 'pancakes' + color.END) prints pancakes but in bold
+attemptCounter = -1 # determines how many attempts the user has to pick the correct option
+
+# --------------- DATA STRUCTURES -----------------
+# Class `color` changes the properties of different pieces of text
+# e.g. => print(color.BOLD + 'pancakes' + color.END) prints pancakes but in bold
 class color:
     BOLD = '\033[1m'
     END = '\033[0m'
     RED = '\033[91m'
     BLUE = '\033[94m'
     CYAN = '\033[96m'
-#function that makes text bold, x is the text you want to be in bold
+
 def bold(x):
     x = color.BOLD + x + color.END
     print(x)
-#makes text red
+
 def red(x):
     x = color.RED + x + color.END
     print(x)
-#makes text blue
+
 def blue(x):
     x = color.BLUE + x + color.END
     print(x)
-    #makes text cyan
+
 def cyan(x):
     x = color.CYAN + x + color.END
     print(x)
-#A quicktime event for typing in a specific word in a specific time, x is the word and y is the time
-def typequick(x,y):
-    print("Quickly! Type in the word",x)
-    typequickans = input()
-    y = time.time()
-#the stats of the player character
-hero  = {"name": 'Hero',
-'lvl' : 1,
-'xp' : 0,
-#how much xp is needed to level up
-'lvlNext' : 25,
-'money' : 0,
-'stats' : {
-    'str' : 1,
-    'int' : 1,
-    'hp'  : 50,
-    'atk' : [5, 18],
-    'luck' : 2,
-    'mp' : 30
-}}
-tutorialEnemy = {
+
+# Dictionary that holds the stats of the player character
+hero  = {
+    "name": 'Hero',
+    'lvl' : 1,
+    'xp' : 0,
+    'lvlNext' : 25, #how much xp is needed to level up
+    'money' : 0,
+    'stats' : {
+        'str' : 1,
+        'int' : 1,
+        'hp'  : 50,
+        'atk' : [5, 18],
+        'luck' : 2,
+        'mp' : 30
+    }
+}
+
+# Dictionary that holds the stats of the enemy character
+enemy = {
     "name" : "Apostle",
     "lvl" : "2",
     "xp" : '0',
@@ -63,11 +66,29 @@ tutorialEnemy = {
     'atk' : [5, 16],
     'luck' : 2,
     'mp' : 30
-}}
-#basic function for dealing damage
+    }
+}
+
+
+# ---------------- UTILITY FUNCTIONS -------------------
+
+# A quicktime event for typing in a specific word in a specific time, x is the word and y is the time
+def typequick(x,y):
+    print("Quickly! Type in the word",x)
+    typequickans = input()
+    y = time.time()
+
+# utility function to clear the screen. This function is OS agnostic
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# basic function for dealing damage
+# TODO: what does this function return?
 def takeDmg(attacker, defender, enemy):
-    dmg = random.randint(attacker['stats']['atk'][0], attacker['stats']['atk'][1])
+    dmg = random.randint(attacker['stats']['atk'][0],
+                         attacker['stats']['atk'][1])
     defender['stats']['hp'] -= dmg
+
     if defender['stats']['hp'] <= 0:
         loser = blue('{} has been slain.'.format(defender['name']))
         if hero['name'] in loser:
@@ -78,7 +99,7 @@ def takeDmg(attacker, defender, enemy):
             if restartChoice == 'Y':
                 blue('Restarting...')
                 time.sleep(3)
-                os.system('cls')
+                clear_screen()
                 time.sleep(2)
                 combatSystem(hero, enemy)
                 return hero['name']
@@ -89,16 +110,17 @@ def takeDmg(attacker, defender, enemy):
                     sys.exit()
         elif hero['name'] not in loser:
             blue(loser)
-            hero['xp'] += tutorialEnemy['reward']
-            hero['money'] += tutorialEnemy['reward']
+            hero['xp'] += enemy['reward']
+            hero['money'] += enemy['reward']
             blue('You earned {} xp and {} runes!'.format(defender['reward']))
             time.sleep(3)
             bold('Clearing screen...')
-            os.system('cls')
+            clear_screen()
             exit(0)
     else:
         blue('{} takes {} damage!'.format(defender['name'], dmg))
-#basic combatSystem
+
+# Basic combatSystem
 def combatSystem(player, enemy):
     global currentEnemy
     currentEnemy = enemy
@@ -112,11 +134,10 @@ def combatSystem(player, enemy):
             print('While you\'re not paying attention, {} takes the opportunity to strike!'.format(enemy['name']))
             takeDmg(enemy, player, currentEnemy)
 
-
-#story portion
+# ---------------- GAME LOGIC  -------------------
 #The function for the opening title. useful for when the game restarts
 def titleScreen():
-    os.system('cls')
+    clear_screen()
     print("""
 ████████╗░██╗░░░░░░░██╗░█████╗░  ░██████╗██╗██████╗░███████╗░██████╗
 ╚══██╔══╝░██║░░██╗░░██║██╔══██╗  ██╔════╝██║██╔══██╗██╔════╝██╔════╝
@@ -183,12 +204,13 @@ def intro():
             print("You have",attemptCounter,"chance(s) left.")
 
 
-#the function for path1 which branches out into different endings/scenarios
+# TODO: the function for path1 which branches out into different endings/scenarios
 def path1():
     print("The choice has been made.")
     print()
     print("(EVERYTHING IS DARK)")
-#the function for path2 which brances out into different ending / scenarios
+
+# TODO: the function for path2 which branches out into different ending / scenarios
 def path2():
     global attemptCounter
     attemptCounter += 4
@@ -221,9 +243,10 @@ def path2():
     bold('What is your choice?')
     print(color.BOLD + 'Option #1:' + color.END, 'Take the offer from NELINA.')
     print(color.BOLD + 'Option #2:' + color.END, 'Decline the offer from NELINA.')
+
     while attemptCounter >= 0:
         attemptCounter -= 1
-        path2Choice1 = input()
+        path2Choice1 = int(input()) # you forgot to cast this to an `int` type. Remember that input() returns a `str` and you need an `int`
         if path2Choice1 == 1:
             attemptCounter = -1
             print()
@@ -240,6 +263,7 @@ def path2():
             print()
             print("Please type one of the given options.")
             print("You have",attemptCounter,"chance(s) left.")
+
 def path2_1():
     print()
     cyan('"Then it\'s settled."')
@@ -249,121 +273,77 @@ def path2_1():
     print('You follow the trial that NELINA left to guide you.')
     print('You see a ')
     print('All of a sudden the light shines more than 10x brighter.')
-    print('') 
+    print('')
+
 def path2_2():
-    print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-print("\n"+"The year is 2018 and you live in a modern world similar to the one individuals live in today. But there seems to be something off...")
-name = input("Please type the name you will be called throughout the game: ")
-print("Hello,",name+"!")
-while True:
-    side = input("\nWhich side of the law do you want to be on? (Type With or Against): ").capitalize()
-    if side == "With":
-        os.system('cls')
-        break
-    elif side == "Against":
-        os.system('cls')
-        break
-    else:
-        print("Please type one of the given options")
-if side == "With":
-    print("\n"+'\n'+'\n'+"You are a regular person in a regular suburban neighborhood. You constantly feel trapped in a cycle of consumerism with each passing day")
-    print("You have a dream about a blue fairy like creature with an enchanting voice, chanting some sacred language that you never heard. There's something alluring about it's voice, it almost feels familiar..")
-    print(color.CYAN + '???:' + color.END,"ИЖДВβψΣχΦΩπσγ, ИЖДВβψΣχΦΩπσγ¥§◎Штю€Ξ ВβψΣχΦΩπ ΦΩ innaR.\n")
-    print("You're abrubtly woken up by a sharp noise. It appears to be your phone call, but it's from an unknown caller...")
-    #choice 1 var
-    print(color.BOLD + "What do you do?\n"+color.END + color.BOLD + '1:' + color.END,"Pick up the phone\n" + color.BOLD + '2:' + color.END,"Look around the house\n"+color.BOLD +'3:'+color.END,"Go back to sleep")
+    print() 
+    print("\n"+"The year is 2018 and you live in a modern world similar to the one individuals live in today. But there seems to be something off...")
+    name = input("Please type the name you will be called throughout the game: ")
+    print("Hello,",name+"!")
     while True:
-        withchoice1 = int(input())
-        if withchoice1 == 1:
-            print("\nYou pick up the phone and hear a mysterious voice ushering random numbers and equations")
-            print(color.RED + 'unknown:' + color.END + "49.131883. 9.160123. 49 7' 54.7788. 9 9' 36.4428")
-            break                                                                    
-        elif withchoice1 == 2:
-            print("You look around your house and find that you're mom who is usually in the living room watching TV is gone. Yet the TV is on her favorite channel...something is off...")
+        side = input("\nWhich side of the law do you want to be on? (Type With or Against): ").capitalize()
+        if side == "With":
+            clear_screen()
             break
-        elif withchoice1 == 3:
-            print("You tell the caller to fuck off, thinking it's a scam. As you lie back in bed trying to remember that dream, you get a call back from your employer that you previously had an interview with.")
+        elif side == "Against":
+            clear_screen()
             break
         else:
-            print("Please type 1, 2 or 3")
-        #next steps if player chooses to pick up the phone
+            print("Please type one of the given options")
+    if side == "With":
+        print("\n"+'\n'+'\n'+"You are a regular person in a regular suburban neighborhood. You constantly feel trapped in a cycle of consumerism with each passing day")
+        print("You have a dream about a blue fairy like creature with an enchanting voice, chanting some sacred language that you never heard. There's something alluring about it's voice, it almost feels familiar..")
+        print(color.CYAN + '???:' + color.END,"ИЖДВβψΣχΦΩπσγ, ИЖДВβψΣχΦΩπσγ¥§◎Штю€Ξ ВβψΣχΦΩπ ΦΩ innaR.\n")
+        print("You're abrubtly woken up by a sharp noise. It appears to be your phone call, but it's from an unknown caller...")
+        #choice 1 var
+        print(color.BOLD + "What do you do?\n"+color.END + color.BOLD + '1:' + color.END,"Pick up the phone\n" + color.BOLD + '2:' + color.END,"Look around the house\n"+color.BOLD +'3:'+color.END,"Go back to sleep")
+        while True:
+            withchoice1 = int(input())
             if withchoice1 == 1:
-                while True:
-                    print(color.BOLD + 'What is your next move?\n' + color.END + color.BOLD + '1:' + color.END + 'Say something\n' + color.BOLD + '2:' + color.END + 'Search up the numbers' + color.BOLD + '3:' + color.END + 'Hang up')
-                #the third choice leads to the bad ending
-                    withchoice2 = int(input())
-                    if withchoice2 == 1:
-                        input("What do you wish to say?\n")
-                        print(color.RED + 'unknown:' + color.END,"*laughs maniacally* You really have no idea do you?......think geographically.")
-                    elif withchoice2 == 2:
-                        print("You search up the numbers on google and it shows a google maps link to an abandon warehouse that you and your friends would explore as kids")
-                    elif withchoice2 == 3:
-                        print("You hang up the phone but the phone continues to rings again from that same unknown caller.")
-                        break
-                    else:
-                        print("Please type 1, 2 or 3")
-            if withchoice2 == 3:
-                #this series of choices leads to the bad ending
-                while True:
-                    print(color.BOLD + "What do you do now?\n" + color.END + color.BOLD + '1:' + color.END, 'Destroy your phone.')
-                    badendingchoice1 = int(input())
-                    if badendingchoice1 == 1:
-                        print("\n You grab the metal bat from under your bed.")
-                        print("As you're holding the bat in your hand, ready to destroy the problem, you get a text message from the unknown caller.")
-                        print(color.RED + 'unknown: dont do it.' + color.END)
-                        print("You do it anyway and batter your helpless phone screen as it shatters into pieces.")
-                        for x in range(1,10):
-                            print(color.BLUE + 'Cellphone' + color.END, 'BEEP')
-                    else:
-                        print("Pick your choice : )")        
-"""
-                
+                print("\nYou pick up the phone and hear a mysterious voice ushering random numbers and equations")
+                print(color.RED + 'unknown:' + color.END + "49.131883. 9.160123. 49 7' 54.7788. 9 9' 36.4428")
+                break
+            elif withchoice1 == 2:
+                print("You look around your house and find that you're mom who is usually in the living room watching TV is gone. Yet the TV is on her favorite channel...something is off...")
+                break
+            elif withchoice1 == 3:
+                print("You tell the caller to fuck off, thinking it's a scam. As you lie back in bed trying to remember that dream, you get a call back from your employer that you previously had an interview with.")
+                break
+            else:
+                print("Please type 1, 2 or 3")
+            #next steps if player chooses to pick up the phone
+                if withchoice1 == 1:
+                    while True:
+                        print(color.BOLD + 'What is your next move?\n' + color.END + color.BOLD + '1:' + color.END + 'Say something\n' + color.BOLD + '2:' + color.END + 'Search up the numbers' + color.BOLD + '3:' + color.END + 'Hang up')
+                    #the third choice leads to the bad ending
+                        withchoice2 = int(input())
+                        if withchoice2 == 1:
+                            input("What do you wish to say?\n")
+                            print(color.RED + 'unknown:' + color.END,"*laughs maniacally* You really have no idea do you?......think geographically.")
+                        elif withchoice2 == 2:
+                            print("You search up the numbers on google and it shows a google maps link to an abandon warehouse that you and your friends would explore as kids")
+                        elif withchoice2 == 3:
+                            print("You hang up the phone but the phone continues to rings again from that same unknown caller.")
+                            break
+                        else:
+                            print("Please type 1, 2 or 3")
+                if withchoice2 == 3:
+                    #this series of choices leads to the bad ending
+                    while True:
+                        print(color.BOLD + "What do you do now?\n" + color.END + color.BOLD + '1:' + color.END, 'Destroy your phone.')
+                        badendingchoice1 = int(input())
+                        if badendingchoice1 == 1:
+                            print("\n You grab the metal bat from under your bed.")
+                            print("As you're holding the bat in your hand, ready to destroy the problem, you get a text message from the unknown caller.")
+                            print(color.RED + 'unknown: dont do it.' + color.END)
+                            print("You do it anyway and batter your helpless phone screen as it shatters into pieces.")
+                            for x in range(1,10):
+                                print(color.BLUE + 'Cellphone' + color.END, 'BEEP')
+                        else:
+                            print("Pick your choice : )")
+
+
+# ---------------- MAIN GAME LOOP  -------------------
+
+if __name__ == "__main__":
+    intro()
